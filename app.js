82b72c6fdb6707ad10f170ea90a363281a09ddbc
Marcos <marcoscruz1992@gmail.com>
2016-03-27 17:05:44 -0400

    fixed thumbnail sizing, added row/grid functionality

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 $(document).ready(function(){
+    //add endpoint to variable
     var flickerAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+    //add specific location retrive photos from
     var location = "Puerto Rico";
+    //Query parameters
     var flickrOptions = {
       tags: location,
       format: "json"
     };
+    //Callback function to build html and inserts data from Flickr
     function displayPhotos(data){
+      //photoHTML variable holds the whole photo-grid HTML
       var photoHTML = '<div class="row photo-grid">';
+      //loops through each item from json array
       $.each(data.items, function(i, photo){
-        if(i !== 0 && i % 5 !== 0 ){
-          photoHTML += '<div class="col-sm-4 col-md-3 photo-container">';
+        //Case statement to break insert new row after 3 elements in place
+        if(i !== 0 && i % 4 !== 0 ){
+          //replace default thumbnail size to a bigger one
+          var photoUrl = photo.media.m.replace(/m.jpg/i,'c.jpg');
+          photoHTML += '<div class="col-sm-6 col-md-4 photo-container">';
           photoHTML += '<div class="hovereffect">';
-          photoHTML += '<img src=" '+photo.media.m + '" class="img-responsive"> ';
+          photoHTML += '<img src=" '+photoUrl+ '" class="img-responsive"> ';
           photoHTML += '<div class="overlay">';
-          photoHTML += '<h6><i class="fa fa-camera"></i>' +photo.author.slice(17) + '</h6>';
+          photoHTML += '<h2><i class="fa fa-camera"></i>' +photo.author.slice(17) + '</h2>';
           photoHTML += '<a href="' + photo.link + '" class="info" target="_blank">Download</a>';
           photoHTML += '</div>';
           photoHTML += '</div>';
@@ -22,10 +31,51 @@ $(document).ready(function(){
           photoHTML += '</div>';
           photoHTML += '<div class="row photo-grid">';
         }
-
       });
+      //Injects HTML to website
       $("#test").html(photoHTML);
-      console.log("hello?");
     }
+
+    //Callback function to build html and inserts data from Flickr
+    function displayPhotosGrid(data){
+      //photoHTML variable holds the whole photo-grid HTML
+      var photoHTML = '<div class="row photo-grid">';
+      //loops through each item from json array
+      $.each(data.items, function(i, photo){
+        //Case statement to break insert new row after 3 elements in place
+        if(i !== 0 && i % 4 !== 0 ){
+          //replace default thumbnail size to a bigger one
+          var photoUrl = photo.media.m.replace(/m.jpg/i,'b.jpg');
+          photoHTML += '<div class="col-sm-6 col-md-12 photo-container">';
+          photoHTML += '<div class="hovereffect">';
+          photoHTML += '<img src=" '+photoUrl+ '" class="img-responsive center-block"> ';
+          photoHTML += '<div class="overlay">';
+          photoHTML += '<h2><i class="fa fa-camera"></i>' +photo.author.slice(17) + '</h2>';
+          photoHTML += '<a href="' + photo.link + '" class="info" target="_blank">Download</a>';
+          photoHTML += '</div>';
+          photoHTML += '</div>';
+          photoHTML += '</div>';
+        } else {
+          photoHTML += '</div>';
+          photoHTML += '<div class="row photo-grid">';
+        }
+      });
+      //Injects HTML to website
+      $("#test").html(photoHTML);
+    }
+    //getJson callback function
     $.getJSON(flickerAPI, flickrOptions, displayPhotos);
+
+    // When one-grid is click images take a whole row
+    $("#one-grid").click(function(){
+      $("span").removeClass('active');
+      $(this).addClass('active');
+      $.getJSON(flickerAPI, flickrOptions, displayPhotosGrid);
+    });
+    // When two-grid is click images take a whole row
+    $("#column-grid").click(function(){
+      $("span").removeClass('active');
+      $(this).addClass('active');
+      $.getJSON(flickerAPI, flickrOptions, displayPhotos);
+    });
 });
